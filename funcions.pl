2a3f62f0af9af6e7ba8a3c848b62ac2fefafa873
Jordi Muñoz <101974385+jordimunozflorensa@users.noreply.github.com>
2024-05-20 18:53:56 +0200

    Add files via upload

---
+++ funcions.pl
@@ -0,0 +1,16 @@
+member(X, L).
+nth1(K, L, F). % K = 1, F = L[1]
+last(L, X).
+append(L1, L2, R).
+sum_list(L, S).
+sort(LNO, LO).
+reverse(L, R).
+write(...).
+length(L, X).
+select(X, L, R). % R = L - {X}
+permutation(L, P).
+subtract(L, S1, S2). % S2 = L - S1
+
+% !
+% fail
+% nl
